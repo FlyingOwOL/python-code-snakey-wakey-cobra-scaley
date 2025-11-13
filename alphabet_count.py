952bbaec4ef12de4784a5e90d304a252e86846c7
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-11-14 00:31:02 +0800

    Create alphabet_count.py

---
+++ alphabet_count.py
@@ -0,0 +1,14 @@
+
+str = input("Enter a string: ")
+
+alphabet_count = 0
+
+for char in str:
+    alphabet_count += 1
+print("Number of alphabets in the string:", alphabet_count)
+
+'''
+    this is a comment
+'''
+
+# this is another comment
